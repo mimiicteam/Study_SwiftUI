fb1202472090edc34cdd82044c197c9918307c51
NGUYEN MINH DUC <mimiicteam@gmail.com>
2022-03-30 16:05:33 +0700

    Better Rest

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by NGUYEN MINH DUC on 29/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
