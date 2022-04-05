11450bb42f2865b3253ed997890cefec304e5c11
NGUYEN MINH DUC <mimiicteam@gmail.com>
2022-04-05 11:26:00 +0700

    Create iExpense

---
+++ iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by NGUYEN MINH DUC on 05/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
