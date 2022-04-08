ca4a08c2a5053841a88c9d3760dfe4e520da0b38
NGUYEN MINH DUC <mimiicteam@gmail.com>
2022-04-08 09:53:13 +0700

    Create Moonshot

---
+++ Moonshot/Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by NGUYEN MINH DUC on 08/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
