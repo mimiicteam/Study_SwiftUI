bbb49c483b22c7ec580eb965f417184ed6bf82ca
NGUYEN MINH DUC <mimiicteam@gmail.com>
2022-04-11 21:15:28 +0700

    Mission View

--- Moonshot/Moonshot/Views/MissionView.swift
+++ Moonshot/Moonshot/Views/MissionView.swift
@@ -8,13 +8,55 @@
 import SwiftUI
 
 struct MissionView: View {
+    
+    let mission: Missions
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader { geometry in
+            ScrollView {
+                VStack {
+                    Image(mission.image)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(maxWidth: geometry.size.width * 0.6)
+                        .padding(.top)
+                    
+                    Text(mission.displayName)
+                        .foregroundColor(.black)
+                        .font(.subheadline.bold())
+                        .frame(width: 100, height: 30)
+                        .background(.white)
+                        .cornerRadius(30)
+                        .padding(5)
+                    
+                    Text(mission.formattedLaunchDate)
+                        .foregroundColor(.white)
+                        .padding(5)
+                    
+                    VStack(alignment: .leading) {
+                        Text("Mission Hightlights")
+                            .foregroundColor(.white)
+                            .font(.title.bold())
+                            .padding(.bottom, 5)
+                        
+                        Text(mission.description)
+                            .foregroundColor(.white)
+                    }
+                    .padding(.horizontal)
+                }
+                .padding(.bottom)
+            }
+        }
+        .navigationTitle(mission.displayName)
+        .navigationBarTitleDisplayMode(.inline)
+        .background(.darkBackground)
     }
 }
 
 struct MissionView_Previews: PreviewProvider {
+    static let missions: [Missions] = Bundle.main.decode("missions.json")
+    
     static var previews: some View {
-        MissionView()
+        MissionView(mission: missions[0])
+            .preferredColorScheme(.dark)
     }
 }
