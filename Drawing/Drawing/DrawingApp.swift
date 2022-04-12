9ab2bcba37e4cc2a84bc12787bf16754d7ff1430
NGUYEN MINH DUC <mimiicteam@gmail.com>
2022-04-12 13:17:22 +0700

    Create Drawing

---
+++ Drawing/Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by NGUYEN MINH DUC on 12/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
